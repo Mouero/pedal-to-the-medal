3455c18ae33b423385ba8bd747c9590e5b2a8a7b
Ostalf <ostalfgladyre@gmail.com>
2023-10-05 18:26:05 +0700

    add openapi generator and getStoreOderByIdTest

--- src/test/java/Study.java
+++ src/test/java/Study.java
@@ -1,19 +1,26 @@
-import com.beust.ah.A;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
-import dto.*;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.SneakyThrows;
-import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import petstore.model.Category;
+import petstore.model.Order;
+import petstore.model.Pet;
+import petstore.model.Tag;
 
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.LinkedHashMap;
-import java.util.Set;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.util.List;
 
 public class Study {
 
@@ -23,134 +30,67 @@ public class Study {
 
     @SneakyThrows
     @Test
-    public void getPetId() {
+    public void getPetIdShouldReturnPetTest() {
         //setup
-        RestTemplate postPet = new RestTemplate();
         RestTemplate restTemplate = new RestTemplate();
         UriComponentsBuilder baseUri = UriComponentsBuilder.fromHttpUrl(url).port(port).path(basePath);
 
-        URI uriPost = baseUri.path("/pet").build().toUri();
-        URI uriGet = baseUri.path("/pet/0").build().toUri();
+        String uriPost = baseUri.build() + "/pet";
+        String uriGet = baseUri.build() + "/pet/0";
 
         HttpHeaders headers = new HttpHeaders();
         headers.add(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
         headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
 
-        Set photoUrls = new HashSet();
-        photoUrls.add("url1");
-        photoUrls.add("url2");
-
-        Set tags = new HashSet<>();
-        tags.add(
-                PetTag.builder()
-                        .id(1)
-                        .name("Sheltie")
-                        .build());
-        tags.add(
-                PetTag.builder()
-                        .id(2)
-                        .name("Boy")
-                        .build());
-
-        PetMessage petMessage = PetMessage.builder()
-                .id(0)
+        Pet petRequest = new Pet();
+        petRequest
+                .id(0L)
                 .name("Night")
-                .category(PetCategory.builder().id(1).name("Dogs").build())
-                .photoUrls(photoUrls)
-                .tags(tags)
-                .status(PetStatus.sold)
-                .build();
+                .category(new Category().id(0L).name("Dogs"))
+                .photoUrls(List.of("url1", "url2"))
+                .tags(List.of(new Tag().id(0L).name("Sheltie"), new Tag().id(1L).name("Boy")))
+                .status(Pet.StatusEnum.SOLD);
 
-        petMessage.setId(0);
-        petMessage.getId();
-        //setup
 
-        //deserialize to json
         ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
-        String json = ow.writeValueAsString(petMessage);
-        //deserialize to json
+        String jsonRequestBody = ow.writeValueAsString(petRequest);
 
+        restTemplate.exchange(uriPost, HttpMethod.POST, new HttpEntity<>(jsonRequestBody, headers), String.class);
 
-        //post pet data
-        postPet.exchange(uriPost, HttpMethod.POST, new HttpEntity<>(json, headers), String.class);
-        //post pet data
+        Pet getPetIdResponse = restTemplate.exchange(uriGet, HttpMethod.GET, new HttpEntity<>(headers), Pet.class).getBody();
 
-        //get pet data
-        String getPetIdResponse = restTemplate.exchange(uriGet, HttpMethod.GET, new HttpEntity<>(headers), String.class).getBody();
-        //get pet data
-
-        //assert data
-        Assert.assertTrue(getPetIdResponse.contains(String.valueOf(petMessage.getId())));
-        Assert.assertTrue(getPetIdResponse.contains(petMessage.getName()));
-        Assert.assertTrue(getPetIdResponse.contains(String.valueOf(petMessage.getStatus())));
-        //assert data
-
+        Assert.assertEquals(petRequest, getPetIdResponse);
     }
+
 
     @SneakyThrows
     @Test
-    public void postPetId() {
-        RestTemplate postPet = new RestTemplate();
-        URI URI = UriComponentsBuilder.fromHttpUrl(url).port(port).path(basePath).path("/pet").build().toUri();
+    public void getStoreOderByIdTest() {
+        RestTemplate restTemplate = new RestTemplate();
+        UriComponentsBuilder baseUri = UriComponentsBuilder.fromHttpUrl(url).port(port).path(basePath);
+
+        String uriPost = baseUri.build() + "/store/order";
+        String uriGet = baseUri.build() + "/store/order/1";
 
         HttpHeaders headers = new HttpHeaders();
         headers.add(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
         headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
 
-        var s = new LinkedHashMap<>().put("s","s");
-
-        Set photoUrls = new HashSet();
-        photoUrls.add("url1");
-        photoUrls.add("url2");
-
-        Set tags = new HashSet<>();
-        tags.add(
-                PetTag.builder()
-                        .id(1)
-                        .name("tag2")
-                        .build());
-        tags.add(
-                PetTag.builder()
-                        .id(2)
-                        .name("tag3")
-                        .build());
-
-
-        PetMessage petMessage = PetMessage.builder()
-                .id(0)
-                .name("doge")
-                .category(PetCategory.builder().id(1).name("Dogs").build())
-                .photoUrls(photoUrls)
-                .tags(tags)
-                .status(PetStatus.sold)
-                .build();
-
-
+        Order orderRequest = new Order();
+        orderRequest
+                .id(1L)
+                .petId(1978L)
+                .quantity(4)
+                .status(Order.StatusEnum.PLACED)
+                .complete(true);
 
         ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
-        String json = ow.writeValueAsString(petMessage);
+        String jsonRequestBody = ow.writeValueAsString(orderRequest);
 
-        System.out.println(json);
+        restTemplate.exchange(uriPost, HttpMethod.POST, new HttpEntity<>(jsonRequestBody, headers), String.class);
 
-        ResponseEntity<String> resp = postPet.exchange(URI, HttpMethod.POST, new HttpEntity<>(json, headers), String.class);
+        Order getStoreOrderIdResponse = restTemplate.exchange(uriGet, HttpMethod.GET, new HttpEntity<>(headers), Order.class).getBody();
+
+        Assert.assertEquals(orderRequest, getStoreOrderIdResponse);
     }
-
-
-    @Test
-    public void findByStatus() {
-        RestTemplate findByStatus = new RestTemplate();
-        URI URI = UriComponentsBuilder.fromHttpUrl(url).port(port).path(basePath).path("/pet/findByStatus").queryParam("status", "sold").build().toUri();
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.add(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
-
-        ResponseEntity<String> resp = findByStatus.exchange(URI, HttpMethod.GET, new HttpEntity<>(headers), String.class);
-
-        System.out.println(resp);
-
-
-    }
-    //test
-
-
 }
